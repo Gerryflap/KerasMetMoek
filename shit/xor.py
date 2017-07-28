9deb8d36ddf3ecdcd5ac4a01708c2344144e1f59
gerryflap <gerry2211@gmail.com>
2017-07-28 22:58:33 +0200

    Added example

---
+++ shit/xor.py
@@ -0,0 +1,16 @@
+import keras as ks
+import numpy as np
+
+model = ks.models.Sequential()
+model.add(ks.layers.Dense(4, input_dim=2, activation=ks.activations.tanh))
+model.add(ks.layers.Dense(1, activation=ks.activations.tanh))
+
+model.compile(loss=ks.losses.mean_squared_error, optimizer=ks.optimizers.Adam(0.01))
+
+x = np.array([[0,0], [0,1], [1,0], [1,1]])
+y = np.array([[0], [1], [1], [0]])
+
+model.fit(x, y, epochs=1000, batch_size=4)
+
+print(model.predict(x))
+
