fcdf870d8d530d86147852f0c50353484dff9be5
MaukWM <maukmul@gmail.com>
2017-08-01 00:20:31 +0200

    voor boon

--- gerboon/examples/gan/nederlands_gan.py
+++ gerboon/examples/gan/nederlands_gan.py
@@ -3,6 +3,7 @@ import codecs
 import keras as ks
 import numpy as np
 
+from util.math import unison_shuffled_copies
 from util.text import filter_text, text_to_one_hot, one_hot_to_text
 
 word_length = 20
@@ -19,7 +20,7 @@ def split_pad_text(text):
     return text
 
 
-with codecs.open("../../../sources/books/jules_verne.txt", "r", encoding='utf-8') as f:
+with codecs.open("../../../sources/bier/nederlands_bier", "r", encoding='utf-8') as f:
     dutch_words = filter_text(f.read())
     alphabet = set(dutch_words)
     dutch_words = split_pad_text(dutch_words)
@@ -37,20 +38,21 @@ def make_trainable(net, val):
         l.trainable = val
 
 # Build Generator
-gen_opt = ks.optimizers.Adam(0.001)
+gen_opt = ks.optimizers.Adam(0.0005)
 gen_model = ks.models.Sequential()
 gen_model.add(ks.layers.LSTM(64, input_shape=(word_length, 5), return_sequences=True))
-gen_model.add(ks.layers.LSTM(64, return_sequences=True))
-gen_model.add(ks.layers.Dense(len(alphabet), activation=ks.activations.softmax))
+gen_model.add(ks.layers.LSTM(32, return_sequences=True))
+gen_model.add(ks.layers.Dropout(0.5))
+gen_model.add(ks.layers.Dense(len(alphabet), activation=ks.activations.tanh))
 gen_model.compile(optimizer=gen_opt, loss=ks.losses.categorical_crossentropy)
 
 # Build Discriminator
 discr_model = ks.models.Sequential()
 discr_model.add(ks.layers.LSTM(64, input_shape=(word_length, len(alphabet)), return_sequences=True))
-discr_model.add(ks.layers.LSTM(64, return_sequences=True))
-discr_model.add(ks.layers.LSTM(64))
+discr_model.add(ks.layers.LSTM(32, return_sequences=True))
+discr_model.add(ks.layers.LSTM(32))
 discr_model.add(ks.layers.Dense(2, activation=ks.activations.softmax))
-discr_model.compile(optimizer=ks.optimizers.Adam(0.002), loss=ks.losses.categorical_crossentropy)
+discr_model.compile(optimizer=ks.optimizers.SGD(lr=0.002), loss=ks.losses.categorical_crossentropy)
 
 # Build stacked model
 make_trainable(discr_model, False)
@@ -69,12 +71,16 @@ for i in range(1000):
 
     # Train discriminator
     np.random.shuffle(words_one_hot)
-    epoch = np.concatenate([words_one_hot[:epoch_size // 2], words], axis=0)
+    x = np.concatenate([words_one_hot[:epoch_size // 2], words], axis=0)
     y = np.zeros([epoch_size, 2])
     y[:epoch_size // 2, 0] = 1
     y[epoch_size // 2:, 1] = 1
+
+    # unified shuffle
+    x, y = unison_shuffled_copies(x, y)
+
     print("Training discriminative model")
-    discr_model.fit(epoch, y, epochs=1, batch_size=200)
+    discr_model.fit(x, y, epochs=1, batch_size=200)
 
     # Train stack
     noise = np.random.rand(epoch_size, word_length, 5)
